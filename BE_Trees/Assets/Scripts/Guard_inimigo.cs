56bff8ed3598c043a3659206e1ad0a3d23c18a28
GoncaloGMendonca <goncalogm2002@gmail.com>
2022-03-28 19:31:51 +0100

    .

---
+++ BE_Trees/Assets/Scripts/Guard_inimigo.cs
@@ -0,0 +1,230 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Panda;
+
+public class Guard_inimigo : MonoBehaviour
+{
+
+    Vector3 GuardPosition;
+    [SerializeField] NavMeshAgent enemieAgent;
+    [SerializeField] Transform target;
+
+    public string spawnerName;
+    float distanceBtwPlayerandEnemie;
+    float EnemieVisionDistanceMax = 30f;
+    float timer;
+    float maxTimeBeforeFollowPlayer = 0.2f;
+    float maxDistanceFollowing = 100f;
+    public Vector3 directionGiveByAlly;
+
+
+    
+    [Task]
+    bool CanSeePlayer;
+
+    [Task]
+    bool isPatrolling = true; 
+
+    [Task]
+    bool isChasing; 
+
+
+    [Task]
+    bool lostPlayer;
+
+    [Task]
+    bool alertEnemieHelp;
+
+ 
+    [Task]
+    bool Idle = true;
+
+
+    private void Awake()
+    {
+       GuardPosition = this.transform.position; 
+       
+    }
+
+
+    [Task]
+    void Idling()
+    {
+       
+        if (!CanSeePlayer)
+        {
+            if (Idle)
+            {
+                Idle = false;
+                lostPlayer = false;
+                isPatrolling = true;
+                Vector3 x = this.transform.position;
+                enemieAgent.destination = x;
+                enemieAgent.destination = GuardPosition;
+                ThisTask.Succeed();
+            }
+            else
+            {
+            
+            }
+        }
+    }
+
+
+    [Task]
+    void CanSeePlayerTask()
+    {
+        distanceBtwPlayerandEnemie = Vector3.Distance(transform.position, target.position);
+
+        if (distanceBtwPlayerandEnemie <= EnemieVisionDistanceMax)
+        {
+            CanSeePlayer = true;
+            ThisTask.Succeed();
+
+        }
+        else
+        {
+            CanSeePlayer = false;          
+            lostPlayer = true;
+            ThisTask.Succeed();
+        }
+     
+
+    }
+
+    [Task] 
+    void AlertNearEnemies()
+    {
+        if (CanSeePlayer)
+        {
+          
+
+            isChasing = true;
+
+            ThisTask.Succeed();
+        }
+        else
+        {
+            isChasing = false;
+            ThisTask.Fail();
+
+        }
+
+
+
+    }
+
+
+    [Task]
+    void ChaseThePlayerTask()
+    {
+
+        if (isChasing)
+        {
+            
+
+            timer += Time.deltaTime;
+            if (timer >= maxTimeBeforeFollowPlayer)
+            {
+                
+                transform.LookAt(target);
+                Vector3 moveTo = Vector3.MoveTowards(transform.position, target.position, maxDistanceFollowing);
+                enemieAgent.destination = moveTo;
+                directionGiveByAlly = enemieAgent.destination;
+
+            }
+            ThisTask.Succeed();
+
+        }
+        
+
+    }
+
+
+
+
+
+
+ 
+
+    [Task]
+    void GoBackToOriginPosition()
+    {
+        if (isPatrolling)
+        {
+            setDestination(GuardPosition);
+            isPatrolling = false;
+            ThisTask.Succeed();
+        }
+      
+
+    }
+
+
+  
+    [Task]
+    void setDestination(Vector3 Destination)
+    {        
+            enemieAgent.destination = Destination;        
+            ThisTask.Succeed();
+    }
+
+
+  
+
+    [Task]
+
+    void LostPlayerPanic()
+    {
+        if (lostPlayer)
+        {
+            CanSeePlayer = false;
+            alertEnemieHelp = false;
+            ThisTask.Succeed();
+
+        }
+    
+
+    }
+
+
+    [Task]
+
+    void AvoidLost()
+    {
+        
+        lostPlayer = false;
+
+        ThisTask.Succeed();
+    }
+
+    [Task]
+    void CheckingPlayer()
+    {
+        
+        if (lostPlayer)
+        {
+            Vector3 x = this.transform.position;
+            enemieAgent.destination = x;
+            lostPlayer = false;
+            isPatrolling = true;
+            ThisTask.Succeed();
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+    
+}
---
+++ BE_Trees/Assets/Scripts/Patrol_inimigo.cs
@@ -0,0 +1,215 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Panda;
+
+
+public class Patrol_inimigo : MonoBehaviour
+{
+
+
+    [SerializeField] Transform target;
+
+    [SerializeField] UnityEngine.AI.NavMeshAgent enemieAgent;
+
+    [SerializeField] Transform[] destinations;
+    int currentPoint;
+
+
+    float timer;
+    float maxTimeBeforeFollowPlayer = 0.2f;
+    float maxDistanceFollowing = 100f;
+    public Vector3 directionGiveByAlly;
+
+
+    
+    float distanceBtwPlayerandEnemie;
+
+    float EnemieVisionDistanceMax = 30f;
+    float AtackRadius;
+
+
+    
+    float maxAtackDistancemellee = 0.8f;
+    bool isMelleAtack;
+    bool isRangedAtack;
+
+  
+    [Task]
+    bool CanSeePlayer;
+
+    [Task]
+    bool isPatrolling = true; 
+
+    [Task]
+    bool isChasing; 
+
+
+    [Task]
+    bool lostPlayer;
+
+    [Task]
+    bool alertEnemieHelp;
+
+  
+    [Task]
+    bool CanAtackPlayer;
+    [Task]
+    bool CanAtackPlayerClose;
+
+
+
+    private void Start()
+    {
+        AtackRadius = EnemieVisionDistanceMax - 2;
+    }
+
+    [Task]
+    void IsCloseDamage()
+    {
+        if (distanceBtwPlayerandEnemie > AtackRadius)
+        {
+            Debug.Log("CloseDamage!");
+            isMelleAtack = true;
+            isRangedAtack = false;
+        }
+    }
+
+
+
+    [Task]
+    void IsRangedDamage()
+    {
+        if (distanceBtwPlayerandEnemie > AtackRadius / 2)
+        {
+            Debug.Log("RangedDamage!");
+
+            isMelleAtack = false; 
+            isRangedAtack = true;
+        }
+    }
+
+
+    
+    [Task]
+    void CanSeePlayerTask()
+    {
+        
+        distanceBtwPlayerandEnemie = Vector3.Distance(transform.position, target.position);
+
+        if (distanceBtwPlayerandEnemie <= EnemieVisionDistanceMax)
+        {
+            CanSeePlayer = true;
+            isPatrolling = false;
+            alertEnemieHelp = true;
+
+        }
+        else 
+        {
+            CanSeePlayer = false;
+            isPatrolling = true;
+        }
+        ThisTask.Succeed();
+
+    }
+   
+    [Task]
+    void ChaseThePlayerTask()
+    {
+        
+
+        isPatrolling = false;
+        isChasing = true;
+
+        if (CanSeePlayer)
+        {
+            
+
+            timer += Time.deltaTime;
+            if (timer >= maxTimeBeforeFollowPlayer)
+            {  
+                transform.LookAt(target);
+                Vector3 moveTo = Vector3.MoveTowards(transform.position, target.position, maxDistanceFollowing);
+                enemieAgent.destination = moveTo;
+                directionGiveByAlly = enemieAgent.destination;
+
+            }
+
+        }
+        ThisTask.Succeed();
+    }
+
+
+    [Task]
+    void PatrollingAroundPoints()
+    {
+        if (!isMelleAtack && enemieAgent.remainingDistance < 0.5f)
+        {
+            isPatrolling = true;
+            isChasing = false;
+            enemieAgent.destination = destinations[currentPoint].position;
+            UpdateCorrentPoint();
+        }
+        Task.current.Succeed();
+    }
+
+    void UpdateCorrentPoint()
+    {
+        
+        if (currentPoint == destinations.Length - 1)
+        {
+            currentPoint = 0;
+        }
+        else
+        {
+            currentPoint++;
+        }
+
+    }
+
+
+    [Task]
+    void LostPlayerPanic()
+    {
+        lostPlayer = true;
+        ThisTask.Succeed();
+    }
+
+    [Task]
+    void CheckingPlayer()
+    {
+        if (lostPlayer)
+        {
+            Vector3 x = this.transform.position;
+            enemieAgent.destination = x;
+        }
+        ThisTask.Succeed();
+
+    }
+
+    IEnumerator ProcurarAteDesistir()
+    {
+        Debug.Log("A procurar jogador!");
+        yield return new WaitForSeconds(10f);
+    }
+
+
+
+    [Task]
+    void AlertNearEnemies()
+    {
+        
+       
+
+        alertEnemieHelp = false;
+
+        ThisTask.Succeed();
+
+    }
+
+
+
+
+
+}
